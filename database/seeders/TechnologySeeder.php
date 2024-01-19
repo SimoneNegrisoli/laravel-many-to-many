fa2e7b3a60c5b83ce2973541ba1726be7453a810
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-19 15:32:56 +0100

    migration and seeding

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['JavaScript', 'HTML5', 'Laravel', 'PHP', 'CSS3', 'Bootstrap', 'Vue.js'];
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->image = $technology;
+
+            $newTechnology->save();
+        }
     }
 }
